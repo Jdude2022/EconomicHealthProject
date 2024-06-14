54afd302c77dfdbc8566820d7c6c55958755f1ec
John <sleepydude1993@gmail.com>
2024-06-14 12:27:53 -0500

    test

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return 'Hello World!'
+#
+#
+# if __name__ == '__main__':
+#     app.run()
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+import sys
+from src.web.Web import Web
 
-
-if __name__ == '__main__':
-    app.run()
+app = Web.main(sys.argv)
+# app.run()
